57457fae6eaa4147b89ff0e0f948ac183bb90b36
jean-zhou <7774846+jean-zhou@user.noreply.gitee.com>
2022-03-02 15:38:52 +0800

    优化代码

--- 编程题/面试编程题.js
+++ 编程题/面试编程题.js
@@ -8,7 +8,7 @@
  * '(' => false
  * '((()())(()()))' => true
  */
-function solution(str) {
+function solution1(str) {
   if (!str) return true;
   let resArr = [];
   let sArr = str.split('');
@@ -32,10 +32,33 @@ function solution(str) {
   return false;
 }
 
+// 判断多种成对字符是否成对，并且区别左右
+const isParis = (charL, charR, str) => {
+  let count = 0;
+  let resArr = str.split('').filter(i => i === charL || i === charR)
+  // 根据一个测试用例修改 - 根据字符串，如果第一个是反括号，后边一定也是不合规
+  if (resArr[0] === charR) return false
+  resArr.forEach(j => {
+    if (j === charL) {
+      count++
+    } else {
+      count--
+    }
+  })
+  if (count === 0) return true;
+  return false;
+}
+
+function solution(str) {
+  if (!str) return true;
+  return isParis('(', ')', str)
+}
+
+
 // let str = ')()(())'
 // let str = '((()())(()()))'
 let str = '('
 console.log(solution(str))
 
 // const es6 的新定义
-// 如何自己写一个编程题的测试用例
+// 如何自己写一个编程题的测试用例 - assert 断言，但是没有找到具体的代码
